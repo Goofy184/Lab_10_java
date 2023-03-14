8d81529ee78e091aa006ec5f2f48689b84215092
Олег Спас <spas.oleh@gmail.com>
2023-03-14 15:44:33 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import static java.awt.Toolkit.getDefaultToolkit;
+public class Main {
+    public static void main(String[] args) {
+        settingWindowForVariant7(new JFrame(), 600);
+        new Window();
+    }
+    public static void settingWindowForVariant7(JFrame window) {
+        settingWindowForVariant7(window, 0);
+    }
+    public static void settingWindowForVariant7(JFrame window, int leftfield) {
+        Dimension screenSize = getDefaultToolkit().getScreenSize();
+        window.setTitle("Нижнє праве вікно");
+        window.setSize(600, 700);
+        window.setLocation(screenSize.width - window.getWidth() - leftfield, screenSize.height - window.getHeight());
+        window.setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+        window.setFocusableWindowState(true);
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        window.setVisible(true);
+        Panel info = getInfoForVariant7(window);
+        window.add(info);
+        info.revalidate();
+    }
+    public static Panel getInfoForVariant7(JFrame window) {
+        Dimension dimension = window.getSize();
+        Point location = window.getLocation();
+        Panel panel = new Panel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.add(new JLabel("Location: x - " + location.x + " y - " + location.y));
+        panel.add(new JLabel("Title: " + window.getTitle()));
+        panel.add(new JLabel("State: " + window.getCursor().getName()));
+        return panel;
+    }
+}
---
+++ src/Window.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    public Window() {
+        Main.settingWindowForVariant7(this);
+    }
+
+}
